ae0e7df93911b2327734348b9c589a5380cec2ee
Freeze455 <baptiste.parmantier.pro@gmail.com>
2021-10-09 16:15:59 +0200

    :construction: Upgrade command

--- src/commands/MakeEcosystem.ts
+++ src/commands/MakeEcosystem.ts
@@ -1,16 +1,19 @@
-import { CLICommand, BaseAddonCommand } from '@discord-factory/core-next'
+import { CLI, BaseCli } from '@discord-factory/core-next'
 import Addon from '../index'
 import path from 'path'
 import fs from 'fs'
 import Logger from '@leadcodedev/logger'
 
-@CLICommand({
-  name: 'Create ecosystem file',
+@CLI({
   prefix: 'pm2:ecosystem',
-  usages: []
+  description: 'Generate a new ecosystem.config.js file at root project',
+  config: {
+    allowUnknownOptions: false,
+    ignoreOptionDefaultValue: false
+  }
 })
-export default class MakeEcosystem extends BaseAddonCommand<Addon> {
-  public async run (filename: string): Promise<void> {
+export default class MakeEcosystem extends BaseCli<Addon> {
+  public async run (): Promise<void> {
     const targetFile = path.join(process.cwd(), 'ecosystem.config.js')
 
     const templateFile = await fs.promises.readFile(
